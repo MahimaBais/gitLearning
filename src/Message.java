e1c8e756179cec46676249635b3bd787c5334cc3
MahimaBais <35098226+MahimaBais@users.noreply.github.com>
2018-01-05 15:36:25 +0530

    Update Message.java

--- src/Message.java
+++ src/Message.java
@@ -17,8 +17,7 @@ public class Message {
     }
     
     public void errorMessage(){
-         System.out.println("Check your operations");
-            System.out.println("Check your operators");
+         System.out.println("Check your operations and operators.");
 
      }
 }
